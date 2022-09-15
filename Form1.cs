755462aa9606ac2fcf4f82c5a9b0962981936d01
dostalm.04 <dostalm.04@spst.eu>
2022-09-15 11:19:37 +0200

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(openFileDialog1.FileName);
+                int pradku = 0;
+                while(!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    if (char.IsUpper(line[0]) && line.EndsWith(".") && (line[line.Length - 2] != '.')) pradku++;
+                }
+                MessageBox.Show("Řádků začínajících velkým písmenem a končících tečkou je: " + pradku);
+            }
+            else
+            {
+                MessageBox.Show("nebyl vybrán žádný soubor");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+          
+        }
+    }
+}
